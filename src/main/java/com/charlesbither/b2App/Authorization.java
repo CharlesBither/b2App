fa820db870eefc1c170afbd724a983b4a29c57ac
Charles Bither <bither.charles@gmail.com>
2022-12-23 20:55:59 -0800

    Add Authorization

--- src/main/java/com/charlesbither/b2App/Authorization.java
+++ src/main/java/com/charlesbither/b2App/Authorization.java
@@ -1,20 +1,27 @@
 package com.charlesbither.b2App;
 
-import netscape.javascript.JSObject;
 import org.json.JSONObject;
 
-import java.io.*;
-import java.util.Base64;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
-public class b2App {
+public class Authorization {
 
-    public static void main(String[] args) {
+    static public List<String> authorizationList() {
         String applicationKeyId = "004417531fd6d8c0000000002"; // Obtained from your B2 account page.
         String applicationKey = "K004Xn571hrcUVUIt+lJMT3JNN6sWxk"; // Obtained from your B2 account page.
         HttpURLConnection connection = null;
         String headerForAuthorizeAccount = "Basic " + Base64.getEncoder().encodeToString((applicationKeyId + ":" + applicationKey).getBytes());
+
+        ArrayList<String> list = new ArrayList<>();
+
         try {
             URL url = new URL("https://api.backblazeb2.com/b2api/v2/b2_authorize_account");
             connection = (HttpURLConnection)url.openConnection();
@@ -29,12 +36,18 @@ public class b2App {
             String apiUrl = json.getString("apiUrl");
             String authorizationToken = json.getString("authorizationToken");
 
+
+            list.add(apiUrl);
+            list.add(authorizationToken);
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             connection.disconnect();
         }
+        return list;
     }
+
     static public String myInputStreamReader(InputStream in) throws IOException {
         InputStreamReader reader = new InputStreamReader(in);
         StringBuilder sb = new StringBuilder();
